b0bbe8c077639d9eb7941f5daa27aaeac6992a97
kmhttmr <63454679+kmhttmr@users.noreply.github.com>
2020-06-19 22:48:24 +0900

    Update ScoreCalc.js

--- ScoreCalc.js
+++ ScoreCalc.js
@@ -1,3 +1,46 @@
+var MusicLevel ={
+    5:"1",
+    6:"1.025",
+    7:"1.05",
+    8:"1.075",
+    9:"1.1",
+    10:"1.2",
+    11:"1.225",
+    12:"1.25",
+    13:"1.275",
+    14:"1.3",
+    15:"1.4",
+    16:"1.425",
+    17:"1.45",
+    18:"1.475",
+    19:"1.5",
+    20:"1.6",
+    21:"1.65",
+    22:"1.7",
+    23:"1.75",
+    24:"1.8",
+    25:"1.85",
+    26:"1.9",
+    27:"1.95",
+    28:"2",
+    29:"2.1",
+    30:"2.2",
+    31:"2.2"    
+};
+
+var ComboRate = {
+    0:"1.0",
+    5:"1.1",
+    10:"1.2",
+    25:"1.3",
+    50:"1.4",
+    70:"1.5",
+    80:"1.7",
+    90:"2.0"
+};
+
+
+
 var Motif ={
     10:"1",
     3000:"2",
@@ -24,7 +67,6 @@ var Motif ={
     49000:"26",
     52000:"27"
 };
-
 
 var SparkleEffect ={
     0:"9",
@@ -82,30 +124,18 @@ var SkillEffect = {
     Symphony:[0,0,0,0,0,50,0]
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
 function calcCover(){
     var TotalFlame = document.getElementById("MusicSec").value * document.getElementById("PerFrame").value;
-    var VisualSec = document.getElementById("VisualSec").value
-    var VocalSec = document.getElementById("VocalSec").value
-    var CenterSec = document.getElementById("CenterSec").value
-    var DanceSec = document.getElementById("DanceSec").value
-    var LeaderSec = document.getElementById("LeaderSec").value
-    var VisualCov = document.getElementById("VisualCov").value
-    var VocalCov = document.getElementById("VocalCov").value
-    var CenterCov = document.getElementById("CenterCov").value
-    var DanceCov = document.getElementById("DanceCov").value
-    var LeaderCov = document.getElementById("LeaderCov").value
+    var VisualSec = document.getElementById("VisualSec").value;
+    var VocalSec = document.getElementById("VocalSec").value;
+    var CenterSec = document.getElementById("CenterSec").value;
+    var DanceSec = document.getElementById("DanceSec").value;
+    var LeaderSec = document.getElementById("LeaderSec").value;
+    var VisualCov = document.getElementById("VisualCov").value;
+    var VocalCov = document.getElementById("VocalCov").value;
+    var CenterCov = document.getElementById("CenterCov").value;
+    var DanceCov = document.getElementById("DanceCov").value;
+    var LeaderCov = document.getElementById("LeaderCov").value;
 
     var InnerHTML = "<table><th style=\"width:50px;\">秒</th><th style=\"width:50px;\">Vi</th><th style=\"width:50px;\">Vo</th><th style=\"width:50px;\">Ce</th><th style=\"width:50px;\">Da</th><th style=\"width:50px;\">Le</th></table>";
     InnerHTML += "<div style=\"overflow-y:scroll;width:350px;height:500px;\"><table><tbody>";
